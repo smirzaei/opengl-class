ee9e8620e1cf8a5f3362be9a21e4c1743a9a2d73
Soroush Mirzaei <soroush.mirzaei@gmail.com>
2020-06-13 14:20:01 +0430

    First simple program.

--- 01_simple.c
+++ 01_simple.c
@@ -1,7 +1,24 @@
 #include <stdio.h>
+#include <GL/glut.h>
 
-int main() {
-    printf("Hello World\n");
+void display() {
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    glBegin(GL_POLYGON);
+        glVertex2f(-0.5, -0.5);
+        glVertex2f(-0.5, 0.5);
+        glVertex2f(0.5, 0.5);
+        glVertex2f(0.5, -0.5);
+    glEnd();
+
+    glFlush();
+}
+
+int main(int argc, char** argv) {
+    glutInit(&argc, argv);
+    glutCreateWindow("My first OpenGL window.");
+    glutDisplayFunc(display);
+    glutMainLoop();
 
     return 0;
 }
--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-
+mkdir -p bin
 gcc "$1" -o "bin/$1" & ./bin/$1
